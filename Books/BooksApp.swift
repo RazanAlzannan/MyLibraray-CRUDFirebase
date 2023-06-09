36da51b65eb5f43efe00697704001042b632bf49
Razan <razanalzannan@gmail.com>
2023-06-09 23:28:02 +0300

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Razan Mohammed Alzannan on 20/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
